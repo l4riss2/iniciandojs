8c2e5bed68a2f034013940ecaab8b323858bf4f5
joaoleirbag <159553162+JamileDelmondes@users.noreply.github.com>
2024-03-13 11:58:19 -0300

    iniciandojs
    iniciandojs 13 03

---
+++ index.js
@@ -0,0 +1,27 @@
+const nome = "larissa";
+console.log(nome);
+
+const al1 = 10;
+const al2 = 5; // comentário
+const al3 = 8;
+
+const media = (al1+al2+al3)/2
+
+const nomeDoAluno1 = "João";
+const nomeDoAluno2 = "Joãozinho";
+const nomeDoAluno3 = "Joãozão";
+ console.log(nomeDoAluno1);
+
+console.log("A media dos alunos é:",media);
+
+console.log('A media dos alunos foi ${media}');
+
+console.log(typeof al1);
+
+if(media>5){
+    console.log("Parabens a media da turma foi:",media);
+}
+else{
+     console.log("Caro aluno estude mais pq sua media foi baixa",media);
+}
+primeiro desafio 2 depois o desafio 1
